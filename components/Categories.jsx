895c379e422c0ddf457b36e96afc37d88c471d91
Hsiang-Hua Huang <digleg_h@hotmail.com>
2023-05-23 15:49:22 +0800

    feature: add basic setting

---
+++ components/Categories.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Categories = () => {
+  return (
+    <View>
+      <Text>Hello Categories</Text>
+    </View>
+  );
+};
+export default Categories;
